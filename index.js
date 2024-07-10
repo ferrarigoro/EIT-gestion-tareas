9df8fda1fed2fbeb9a4b3ebbff50266a05a127d6
Luis M. Goro <ferrarigoro@gmail.com>
2024-07-10 14:09:04 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const app = require('./app')
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Conectado a la DB')
+
+        app.listen(process.env.SERVER_PORT, () => {console.log('Servidor funcionando en puerto 3000')})
+    })
+    .catch(error => console.log(error))
